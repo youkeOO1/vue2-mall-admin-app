dde58a578cde5482e54c76458793c15b70d67ff7
youkeoo1 <1921380844@qq.com>
2021-06-20 01:39:34 +0800

    优化layout

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 首页左侧菜单的显示与隐藏 false：隐藏 true：显示
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
---
+++ src/views/layout/components/mainHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-header">
+    <!-- 左侧导航栏收缩 -->
+    <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <!-- 面包屑 -->
+    <div class="breadcrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>首页</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <!-- 用户 -->
+    <ul class="user-info">
+      <li>
+        欢迎用户某某某
+        <a-icon type="down" />
+      </li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
